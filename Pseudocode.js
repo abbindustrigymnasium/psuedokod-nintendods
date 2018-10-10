57809d09e83b749273112d39684db32add0f7e7b
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2018-10-10 12:49:02 +0200

    uppgift 3 js

---
+++ Pseudocode.js
@@ -0,0 +1,12 @@
+var Åsa = 20;
+var Lustigahuset = 2;
+var Flygandekvasten = 4;
+var Galnamusen = 3;
+
+var kvot1 = Åsa / Lustigahuset;
+var kvot2 = Åsa / Flygandekvasten;
+var kvot3 = Åsa / Galnamusen;
+
+console.log("Lustigahuset", kvot1);
+console.log("Galnamusen", kvot3);
+console.log("Flygandekvasten", kvot2);
